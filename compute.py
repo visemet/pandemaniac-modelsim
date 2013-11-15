ec4739e7f60e0c65b21b3dcce0bc7a3ca70234e4
Angela Gong <anjoola@anjoola.com>
2013-11-15 12:26:18 -0800

    Floyd-Warshall algorithm now done more efficiently.

--- compute.py
+++ compute.py
@@ -8,77 +8,11 @@ import random
 
 from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import floyd_warshall as fw
-from scipy.sparse.csgraph import shortest_path as scipy_shortest_path
 import numpy as np
 
 MAX_DIST = 293879873
 
-def reindex(adj_list):
-  new_adj_list = {}
-  node_mappings = {}
-  i = 0
-  for node in adj_list.keys():
-    if node not in node_mappings:
-      node_mappings[node] = i
-      i += 1
-    new_node = node_mappings[node]
-    new_adj_list[new_node] = []
-    for neighbor in adj_list[node]:
-      if neighbor not in node_mappings:
-        node_mappings[neighbor] = i
-        i += 1
-      new_adj_list[new_node].append(node_mappings[neighbor])
-
-  # Reverse the node mappings to be able to un-index later.
-  node_mappings = dict([(new, old) for (old, new) in node_mappings.iteritems()])
-  #node_mappings = dict(zip(node_mappings.values(), node_mappings.keys()))
-  return (new_adj_list, node_mappings)
-
-
-def floyd_warshall(adj_list):
-  """
-  Function: floyd_warshall
-  ------------------------
-  Finds the shortest path between all nodes using the Floyd-Warshall
-  algorithm.
-  """
-  # Initialize the dictionary of distances.
-  init_dict = dict([(node, MAX_DIST) for node in adj_list.keys()])
-  dist = dict([(node, init_dict) for node in adj_list.keys()])
-  nodes = dist.keys()
-  for i in nodes:
-    for j in nodes:
-      # If finding distance between the same vertex, the distance is 0.
-      if i == j:
-        dist[i][j] = 0
-      # If there is an edge, the distance is 1.
-      elif j in adj_list[i]:
-        dist[i][j] = 1
-
-  # Update distances.
-  for i in nodes:
-    for j in nodes:
-      for k in nodes:
-        new_dist = dist[j][i] + dist[i][k]
-        if new_dist < dist[j][k]:
-          dist[j][k] = new_dist
-  return dist
-
-
-def shortest_path(adj_list):
-  """
-  Function: shortest_path
-  -----------------------
-  TODO
-  """
-  n = len(adj_list)
-  matrix = np.zeros((n, n), dtype=np.int)
-  for n1 in adj_list.keys():
-    for n2 in adj_list[n1]:
-      matrix[n1][n2] = 1
-  dists = fw(csr_matrix(matrix), directed=False, unweighted=True)
-  return dists.tolist()
-
+# ----------------------------- Computation Methods ---------------------------
 
 def by_degree(adj_list, num):
   """
@@ -106,19 +40,14 @@ def by_closeness(adj_list, num):
   """
   # Need to re-index the adjacency list.
   (adj_list, node_mappings) = reindex(adj_list)
-  dist = shortest_path(adj_list)
+  (dist, _) = shortest_path(adj_list)
 
   # List of tuples of the form (closeness, node).
   closeness = [(sum([l for l in dist[node] if l < MAX_DIST]), node) \
     for node in range(len(dist))]
   closeness = sorted(closeness, key=lambda tup: tup[0], reverse=True)
 
-
   return unindex([x[1] for x in closeness[0:num]], node_mappings)
-
-
-def unindex(lst, node_mappings):
-  return [node_mappings[x] for x in lst]
 
 
 def by_betweenness(adj_list, num):
@@ -134,10 +63,36 @@ def by_betweenness(adj_list, num):
     --------------------------------------
              ((n - 1) choose 2)
   """
+  # Need to re-index the adjacency list.
+  (adj_list, node_mappings) = reindex(adj_list)
+  print "done"
+  #(dist, pred) = shortest_path(adj_list)
+  (dist, pred) = floyd_warshall(adj_list)
+  print "supdeond"
+
+
+  print str(pred[0][0:40])
+  """
+  # Predecessors is structured as an N x N matrix. The ith row represents
+  # shortest paths from point i. Each entry (i, j) represents the index
+  # of the previous node in the shortest path from i to j.
+
+  # Array to store the number of shortest paths from j to k. The entry
+  # (j, k) is the number of shortest paths from j to k.
+  P_jk = [[0] * len(adj_list)] * len(adj_list)
+  # Array to store the number of shortest paths that pass through i.
+  P_i = [0] * len(adj_list)
+  for j in xrange(len(adj_list)):
+    for k in xrange(len(adj_list)):
+
+      print str(pred[0])
+  # 
+  """
+  
   between = []
   # TODO
   between = sorted(between, key=lambda tup: tup[0], reverse=True)
-  return [x[1] for x in between[0:num]]
+  return unindex([x[1] for x in between[0:num]], node_mappings)
 
 
 def by_clustering(adj_list, num):
@@ -157,8 +112,7 @@ def by_clustering(adj_list, num):
     neighbors = adj_list[node]
     for i in xrange(len(neighbors)):
       for j in xrange(i + 1, len(neighbors)):
-        if neighbors[i] != neighbors[j] and \
-          neighbors[j] in adj_list[neighbors[i]]:
+        if neighbors[j] in adj_list[neighbors[i]]:
           triangles += 1
     return triangles
 
@@ -181,7 +135,6 @@ def by_clustering(adj_list, num):
     cluster.append((clustering, node))
 
   cluster = sorted(cluster, key=lambda tup: tup[0], reverse=True)
-  print cluster[0], cluster[-1]
   return [x[1] for x in cluster[0:num]]
 
 
@@ -192,6 +145,84 @@ def by_random(adj_list, num):
   Selects num random nodes.
   """
   return random.sample(adj_list.keys(), num)
+
+
+# ------------------------------- Helper Methods ------------------------------
+
+def reindex(adj_list):
+  new_adj_list = {}
+  node_mappings = {}
+  i = 0
+  for node in adj_list.keys():
+    if node not in node_mappings:
+      node_mappings[node] = i
+      i += 1
+    new_node = node_mappings[node]
+    new_adj_list[new_node] = []
+    for neighbor in adj_list[node]:
+      if neighbor not in node_mappings:
+        node_mappings[neighbor] = i
+        i += 1
+      new_adj_list[new_node].append(node_mappings[neighbor])
+
+  # Reverse the node mappings to be able to un-index later.
+  node_mappings = dict([(new, old) for (old, new) in node_mappings.iteritems()])
+  #node_mappings = dict(zip(node_mappings.values(), node_mappings.keys()))
+  return (new_adj_list, node_mappings)
+
+
+def unindex(lst, node_mappings):
+  return [node_mappings[x] for x in lst]
+
+
+def floyd_warshall(adj_list):
+  """
+  Function: floyd_warshall
+  ------------------------
+  Finds the shortest path between all nodes using the Floyd-Warshall
+  algorithm. This is very slow.
+  """
+  N = len(adj_list)
+
+  # Initialize the dictionary of distances (which is an N x N matrix).
+  dist = [[MAX_DIST] * N] * N
+  for i in xrange(N):
+    # Distance between vertex to itself is 0.
+    dist[i][i] = 0
+    # If there is an edge, the distance is 1.
+    for j in adj_list[i]:
+        dist[i][j] = 1
+
+  print "aaa"
+  # Update distances.
+  for i in xrange(N):
+    for j in xrange(N):
+      for k in xrange(N):
+        new_dist = dist[j][i] + dist[i][k]
+        if new_dist < dist[j][k]:
+          dist[j][k] = new_dist
+  return (dist, [])
+
+
+def shortest_path(adj_list):
+  """
+  Function: shortest_path
+  -----------------------
+  Finds the all-pairs shortest path using the SciPy library in conjuction with
+  NumPy.
+
+  returns: A tuple containing the (distances, predecessor matrix) where
+           predecessor matrix contains information on the actual shortest path
+           between nodes.
+  """
+  n = len(adj_list)
+  matrix = np.zeros((n, n), dtype=np.int)
+  for n1 in adj_list.keys():
+    for n2 in adj_list[n1]:
+      matrix[n1][n2] = 1
+  (dists, preds) = fw(csr_matrix(matrix), directed=False, unweighted=True, \
+    return_predecessors=True)
+  return (dists.tolist(), preds.tolist())
 
 
 if __name__ == "__main__":
@@ -216,7 +247,7 @@ if __name__ == "__main__":
     result = by_betweenness(adj_list, num)
   elif method == "clustering":
     result = by_clustering(adj_list, num)
-  else:#if method == "random":
+  elif method == "random":
     result = by_random(adj_list, num)
 
   output = open("nodes.txt", "w")
